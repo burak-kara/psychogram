7bb28ef9c9aa21d4952b5be8eca963d33e498eb0
Burak Kara <git-8f4cb@burak-kara.dev>
2020-03-24 21:07:25 +0300

    created project structure and routing
    Signed-off-by: Burak Kara <burak.kara@ozu.edu.tr>

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Profile from "../pages/profile/Profile";
+import Home from "../pages/home/Home";
 
 export default class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <Router>
                 <Switch>
                     <Route
                         exact path="/"
+                        render={(props) => <Home/>}
+                    />
+                    <Route
+                        exact path="/profile"
                         render={(props) => <Profile/>}
                     />
                 </Switch>
-            </BrowserRouter>
+            </Router>
         );
     }
 }
