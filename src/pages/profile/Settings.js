0f3fa917d9ab7f5e25133656f312fb87d6892ac8
Burak Kara <git-8f4cb@burak-kara.dev>
2020-03-29 18:40:15 +0300

    added material-ui to the project. added DialogWindow.js and Alert.js components to common use cases
    Signed-off-by: Burak Kara <burak.kara@ozu.edu.tr>

--- src/pages/profile/Settings.js
+++ src/pages/profile/Settings.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import DialogWindow from "../../components/DialogWindow";
+import {Typography} from "@material-ui/core";
 
-const Settings = () => {
+const Settings = (props) => {
+    const renderContent = () => {
+        return (
+            <>
+                <Typography gutterBottom>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                    in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </Typography>
+                <Typography gutterBottom>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                    lacus vel augue laoreet rutrum faucibus dolor auctor.
+                </Typography>
+                <Typography gutterBottom>
+                    Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+                    scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+                    auctor fringilla.
+                </Typography>
+            </>
+        );
+    };
 
+    const renderActions = () => {
+        return (
+            <>
+                <button className="btn btn-secondary" onClick={props.handleClose}>
+                    Vazgeç
+                </button>
+                <button className="btn btn-primary" onClick={props.handleSave}>
+                    Kaydet
+                </button>
+            </>
+        );
+    };
+    return (
+        <DialogWindow
+            title="Settings"
+            content={renderContent()}
+            actions={renderActions()}
+            open={props.open}
+            handleClose={props.handleClose}
+        />
+    );
 };
 
 export default Settings;
