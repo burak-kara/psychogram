67c40519d5e52b277105000d5921c771d249aa85
elifgocum <elif.gocumoglu@ozu.edu.tr>
2020-04-02 22:42:14 +0300

    reformatting all pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Main from "./pages/home/Main";
+import App from "./pages/home/Main";
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import "./assets/styles/main.scss";
 
-ReactDOM.render(<Main/>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
 serviceWorker.register();
--- src/pages/home/Login.js
+++ src/pages/home/Login.js
@@ -1,75 +1,54 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
+import React from "react";
+import {Link, Redirect} from "react-router-dom";
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-/*import '../App.css';*/
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
-
-import Main from "./Main";
-import Home from './Home'
-import SignUp from "./Signup";
-import Contact from './Contact'
-import Profile from './Profile'
 
 export default class Login extends React.Component {
-
     state = {
         redirect: false
-    }
+    };
     setRedirect = () => {
         this.setState({
             redirect: true
-        })
+        });
         this.renderRedirect()
-    }
+    };
     renderRedirect = () => {
         if (this.state.redirect) {
-            return <Redirect to='/Home' />
+            return <Redirect to='/Home'/>
         }
-    }
+    };
 
     render() {
         return (
             <div>
-
-            <form>
-                <div className="mainTitle" style={{width: '100%',  height: '100%'}}>
-                    <h1>PSYCHOGRAM</h1>
-                </div>
-                <h3>Sign In</h3>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <br></br>
-                <div className="form-group">
-                    <div className="login-with-google">
-                        <a href="https://www.ozyegin.edu.tr">login-with-google</a>
+                <form>
+                    <div className="mainTitle">
+                        <h1>PSYCHOGRAM</h1>
                     </div>
-                </div>
-
-                <div className="form-group">
-                    {this.renderRedirect()}
-                    <button   onClick={this.setRedirect} type="submit" className="btn btn-primary btn-block">Submit</button>
-                </div>
-
-                <p id="noacc text-left">Don't have an acount? <Link id="noaccLink" className="nav-link" to={"/sign-up"}>Sign up</Link></p>
-
-                <p className="forgot-password text-right"> Forgot <a href="#">password?</a> </p>
-
-
-            </form>
-
-         </div>
-
+                    <h3>Sign In</h3>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password"/>
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <div className="login-with-google">
+                            <a href="https://www.ozyegin.edu.tr">Login with Google</a>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        {this.renderRedirect()}
+                        <button onClick={this.setRedirect} type="submit" className="btn btn-primary btn-block">Submit
+                        </button>
+                    </div>
+                    <p id="noacc text-left">Don't have an acount? <Link id="noaccLink" className="nav-link"
+                                                                        to={"/sign-up"}>Sign up</Link></p>
+                </form>
+            </div>
         );
     }
 }
