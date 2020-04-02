a922b2593cbb892e6c35369b864621102539e076
yusayalcin <yusayalcin6@gmail.com>
2020-04-02 20:48:30 +0300

    revision

--- src/components/forumComponents/postComponents/Time.jsx
+++ src/components/forumComponents/postComponents/Time.jsx
@@ -1,21 +1,17 @@
 import React, {Component} from "react";
+import moment from "moment";
 
 class Time extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            today: Date.now()
+            today: moment().format('DD.MM.YYYY')
         }
     }
 
-
     render() {
         return (
-            <p className="currentTime">{((new Intl.DateTimeFormat('en-US', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit'
-            }).format(this.state.today)))}</p>
+            <p className="currentTime">{this.state.today}</p>
         );
     }
 }
