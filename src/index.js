46326361f944f81e0338d2242fa7ff9157bf695d
Burak Kara <burak.kara@ozu.edu.tr>
2020-04-06 22:10:38 +0300

    added EsLint and prettier
    Signed-off-by: burakkkara <burak.kara@ozu.edu.tr>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
-import "./index.css";
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import "./assets/styles/main.scss";
-import "./assets/styles/components/main.css"
+import './assets/styles/main.scss';
+import './assets/styles/components/main.css';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
