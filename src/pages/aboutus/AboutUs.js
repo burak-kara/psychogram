ac12885891ff1eb412e1b2d773debb81c4bf375d
AtaberkU <ataberk.uncuer@ozu.edu.tr>
2020-04-11 21:16:36 +0300

    no message

--- src/pages/aboutus/AboutUs.js
+++ src/pages/aboutus/AboutUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import psycho from '../../assets/logo/psycho.jpg';
 
 const AboutUs = props => {
@@ -6,9 +6,11 @@ const AboutUs = props => {
         <div className="AboutUspage">
             <img id="picLoc" src={psycho} alt="main picture" />
             <h2 id="Aboutus">ABOUT US</h2>
-            <p>We provide online professional and affordable psychiatric help for people who don't have the chance to get
-            face to face help.   </p>
+            <p>
+                We provide online professional and affordable psychiatric help
+                for people who don't have the chance to get face to face help.{' '}
+            </p>
         </div>
     );
 };
-export default AboutUs; 
+export default AboutUs;
