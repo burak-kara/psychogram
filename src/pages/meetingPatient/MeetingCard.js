2f3405ab35125c971823f17ee5eadb9423dc1979
Burak Kara <git-8f4cb@burak-kara.dev>
2020-04-27 15:29:23 +0300

    done responsive design of meeting list. continueing chat feed
    Signed-off-by: burakkkara <burak.kara@ozu.edu.tr>

--- src/pages/meetingPatient/MeetingCard.js
+++ src/pages/meetingPatient/MeetingCard.js
@@ -8,22 +8,18 @@ const MeetingCard = props => {
     const renderDateCol = () => {
         const formatted = message ? dateAsString(message.date) : ' ';
         return (
-            <div className="col-4 col-md-3 date-info">
+            <div className="col date-info">
                 {formatted.includes('ago') ? (
                     <div className="row">
-                        <div className="col-12 font-14 font-weight-light text-right pt-1 pr-2">
-                            <span>
-                                {message
-                                    ? formatDateAsHours(message.date)
-                                    : ' '}
-                            </span>
+                        <div className="col font-14 font-weight-light text-right pt-1 pr-2">
+                            <span>{formatDateAsHours(message.date)}</span>
                         </div>
                     </div>
                 ) : (
                     ''
                 )}
                 <div className="row">
-                    <div className="col-12 font-14 font-weight-lighter text-right pt-1 pr-2">
+                    <div className="col font-14 font-weight-lighter text-right pt-1 pr-2">
                         <span>{formatted}</span>
                     </div>
                 </div>
@@ -34,19 +30,19 @@ const MeetingCard = props => {
     return user ? (
         <div className="container-fluid card">
             <div className="row h-100">
-                <div className="col col-sm-2 d-flex align-items-center justify-content-center">
+                <div className="col picture-container">
                     <Avatar src={user.profilePictureSource}>
                         {`${user.name[0]}${user.surname[0]}`}
                     </Avatar>
                 </div>
-                <div className="col-6 col-md-7 message-info">
+                <div className="col message-info">
                     <div className="row">
-                        <div className="col-12 font-16 font-weight-normal padding-0">
+                        <div className="col name">
                             <span>{`${user.name} ${user.surname}`}</span>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-12 font-14 font-weight-lighter message padding-0">
+                        <div className="col message">
                             <span>{message ? message.message : ' '}</span>
                         </div>
                     </div>
