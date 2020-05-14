b1ea5b80451b138af3b904473793404e31efa7a9
AtaberkU <ataberk.uncuer@ozu.edu.tr>
2020-05-14 16:43:00 +0300

    no message

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,6 +9,7 @@ import { FaUserAlt, FaSignOutAlt } from 'react-icons/fa';
 import { IoIosMore } from 'react-icons/io';
 import { IconContext } from 'react-icons';
 import { withFirebase } from '../_firebase';
+import SignIn from './signPages/sign-in/SignIn';
 
 const Navigation = props => (
     <AuthUserContext.Consumer>
@@ -21,7 +22,7 @@ const Navigation = props => (
                     />
                 </>
             ) : (
-                <NavigationAuth authUser={authUser} firebase={props.firebase} />
+                <NavigationNoAuth firebase={props.firebase} />
             )
         }
     </AuthUserContext.Consumer>
@@ -72,31 +73,35 @@ const NavigationAuth = ({ authUser, firebase }) => {
                             Home
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={ROUTES.FORUM}>
-                            Forum
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={ROUTES.SIGN_IN}>
-                            Sign In
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={ROUTES.MEETINGS}>
-                            Meetings
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={ROUTES.DOCTOR_LIST}>
-                            Doctors
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href={ROUTES.RESERVATIONS}>
-                            Reservations
-                        </a>
-                    </li>
+
+                    {authUser.role === ROLES.PATIENT || ROLES.DOCTOR ? (
+                        <li className="nav-item">
+                            <a className="nav-link" href={ROUTES.FORUM}>
+                                Forum
+                            </a>
+                        </li>
+                    ) : null}
+                    {authUser.role === ROLES.PATIENT || ROLES.DOCTOR ? (
+                        <li className="nav-item">
+                            <a className="nav-link" href={ROUTES.MEETINGS}>
+                                Meetings
+                            </a>
+                        </li>
+                    ) : null}
+                    {authUser.role === ROLES.PATIENT ? (
+                        <li className="nav-item">
+                            <a className="nav-link" href={ROUTES.DOCTOR_LIST}>
+                                Doctors
+                            </a>
+                        </li>
+                    ) : null}
+                    {authUser.role === ROLES.DOCTOR ? (
+                        <li className="nav-item">
+                            <a className="nav-link" href={ROUTES.RESERVATIONS}>
+                                Reservations
+                            </a>
+                        </li>
+                    ) : null}
                 </ul>
                 <ul className="navbar-nav dots">
                     <IconContext.Provider
@@ -130,7 +135,7 @@ const NavigationAuth = ({ authUser, firebase }) => {
                                         authUser.role === ROLES.DOCTOR ||
                                         ROLES.PATIENT
                                             ? ROUTES.PROFILE
-                                            : ROUTES.SIGN_IN
+                                            : null
                                     }
                                 >
                                     Profile
@@ -156,6 +161,94 @@ const NavigationAuth = ({ authUser, firebase }) => {
                             </ListItemIcon>
                             <li>
                                 <SignOut />
+                            </li>
+                        </MenuItem>
+                    </Menu>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+const NavigationNoAuth = ({ firebase }) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [logo, setLogo] = useState('');
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    useEffect(() => {
+        firebase
+            .getLogo()
+            .getDownloadURL()
+            .then(url => {
+                setLogo(url);
+            });
+    }, [firebase]);
+
+    return (
+        <nav className="navbar navbar-expand-lg navigator">
+            <a className="navbar-brand" href="/">
+                <img src={logo} width="50" height="50" alt="" />
+            </a>
+            {/* TODO  button doesnt work import bootstrap js also add redux as dependency*/}
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link " href={ROUTES.LANDING}>
+                            Home
+                        </a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav dots">
+                    <IconContext.Provider
+                        value={{ color: 'white', size: '2em' }}
+                    >
+                        <div>
+                            <IoIosMore onClick={handleClick} />
+                        </div>
+                    </IconContext.Provider>
+                    <Menu
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
+                        }}
+                    >
+                        <MenuItem>
+                            <ListItemIcon>
+                                <FaUserAlt fontSize="small" />
+                            </ListItemIcon>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <ListItemIcon>
+                                <FaSignOutAlt fontSize="small" />
+                            </ListItemIcon>
+                            <li>
+                                <Link to={ROUTES.SIGN_IN}>SignIn</Link>
                             </li>
                         </MenuItem>
                     </Menu>
