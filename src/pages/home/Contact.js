93abdabaf6eeb06551a9d2a6d83d3cebe57f629d
burakkkara <burak.kara@ozu.edu.tr>
2020-05-25 03:51:26 +0300

    app's common pages are fixed. others' views on profile pages are fixed. profile styling is done
    Signed-off-by: burakkkara <burak.kara@ozu.edu.tr>

--- src/pages/home/Contact.js
+++ src/pages/home/Contact.js
@@ -3,11 +3,11 @@ import { withFirebase } from '../../_firebase';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>GOT QUESTIONS?</h2>
-            <p>
-                Ask your question to us via e-mail.
-                <a href="mailto:info@psychogram.com">Email Me</a>
+        <div className="contact">
+            <h2 className="header mb-4">GOT QUESTIONS?</h2>
+            <p className="content pb-5">
+                <p> Ask your question to us via e-mail.</p>
+                <a href="mailto:info@psychogram.com">Email Us</a>
             </p>
         </div>
     );
